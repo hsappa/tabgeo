8b504df8707b869f1436728964c799373f8112c9
Paul Mercurio <j.paul.mercurio@gmail.com>
2018-02-21 02:45:06 -0500

    fixes to readme.md

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require 'logic.php';
 ?>
-<!doctype html>
-<html lang='en'>
+<!DOCTYPE html>
+<html lang='en-US'>
 <head>
-    <title>Tabula Geographica</title>
     <meta charset='utf-8'>
+    <title>Tabula Geographica</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
@@ -101,8 +101,8 @@ require 'logic.php';
                 <?php $i++; ?>
             <?php endforeach ?>
             <?php if ($isTagOpen): ?>
-        </div>
-        <?php endif ?>
+                </div>
+            <?php endif ?>
         </div>
     </article>
 </main>
